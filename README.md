123cc70cbe9981b1fc43a76eceeffd708e5bbe26
Luke Turner <luke@lukedturner.com>
2019-06-30 21:11:15 +1000

    Readify submission - added custom tests to bottom of file

---
+++ README.md
@@ -0,0 +1,12 @@
+This is my submission for the Readify coding challenge (Luke Turner).
+
+My github repo can be found here: https://github.com/luketurnerdev/readify-robot
+
+To run the application, either clone the above repo, or download the robot.js file from this directory.
+
+To run, install the latest version of Node, and run the command: node robot.js.
+
+The file will run in the console with the provided test data, as well as some supplementary test data, which can be uncommented to test.
+
+The robot.js file can be edited inside the run() function in order to test more functionality.
+
--- robot.js
+++ robot.js
@@ -1,59 +1,3 @@
-
-
-// Toy Robot Simulator
-// --------------------
-// Create a library that can read in commands of the following form:
-
-// PLACE X,Y,DIRECTION
-// MOVE
-// LEFT
-// RIGHT
-// REPORT
-
-// . The library allows for a simulation of a toy robot moving on a 5 x 5 square tabletop.
-// . There are no obstructions on the table surface.
-// . The robot is free to roam around the surface of the table, but must be prevented from falling to destruction. Any movement that would result in this must be prevented, however further valid movement commands must still be allowed.
-// . PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST.
-// . (0,0) can be considered as the SOUTH WEST corner and (4,4) as the NORTH EAST corner.
-// . The first valid command to the robot is a PLACE command. After that, any sequence of commands may be issued, in any order, 
-// including another PLACE command. The library should discard all commands in the sequence until a valid PLACE command has been executed.
-// . The PLACE command should be discarded if it places the robot outside of the table surface.
-// . MOVE will move the toy robot one unit forward in the direction it is currently facing.
-// . LEFT and RIGHT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.
-// . REPORT will announce the X,Y and orientation of the robot.
-// . A robot that is not on the table can choose to ignore the MOVE, LEFT, RIGHT and REPORT commands.
-// . The library should discard all invalid commands and parameters.
-
-// Example Input and Output:
-// a)
-// PLACE 0,0,NORTH
-// MOVE
-// REPORT
-// Output: 0,1,NORTH
-
-// b)
-// PLACE 0,0,NORTH
-// LEFT
-// REPORT
-// Output: 0,0,WEST
-
-// c)
-// PLACE 1,2,EAST
-// MOVE
-// MOVE
-// LEFT
-// MOVE
-// REPORT
-// Output: 3,3,NORTH
-
-// - Use your preferred language, platform and IDE to implement this solution.
-// - Your solution should be clean and easy to read, maintain and execute.
-// - There must be a way to supply the library with input data. 
-// - Writing an interface (command prompt or otherwise) is not mandatory.
-// - You should provide sufficient evidence that your solution is complete by, as a minimum, indicating that it works correctly against the supplied test data.
-// - You should provide build scripts or instructions to build and verify the solution.
-// - The code should be original and you may not use any external libraries or open source code to solve this problem, but you may use external libraries or tools for building or testing purposes.
-
 class Robot {
 
     constructor(position = "", direction = []) {
@@ -83,6 +27,7 @@ class Robot {
                 this.hasBeenPlaced = true;
         } else {
             console.log('Please provide a valid position and direction for placement.');
+            //hasBeenPlaced remains false
         }
 
     }
@@ -105,19 +50,16 @@ class Robot {
                 (this.robotPosition[1] === 4) ? this.fallOffError("NORTH") : (this.robotPosition[1] ++, moved = true);
                 break;
             case "SOUTH":
-                (this.robotPosition[1] === 0) ? this.fallOffError("SOUTH") : (this.robotPosition[1] --, moved = true);
+                (this.robotPosition[1] === 0) ? this.fallOffError("SOUTH") : (this.robotPosition[1]--, moved = true);
                 break;
             case "EAST":
-                (this.robotPosition[0] === 4) ? this.fallOffError("EAST") : (this.robotPosition[0] --, moved = true);
+                (this.robotPosition[0] === 4) ? this.fallOffError("EAST") : (this.robotPosition[0]++, moved = true);
                 break;
             case "WEST":
-                (this.robotPosition[0] === 0) ? this.fallOffError("WEST") : (this.robotPosition[1] --, moved = true);
-                break;
+                (this.robotPosition[0] === 0) ? this.fallOffError("WEST") : (this.robotPosition[0]--, moved = true);
             default:
-                console.log("Invalid direction!");
                 break;
             }
-
 
             if (moved) {
                 console.log(`Moved one spot ${this.robotDirection}!`);
@@ -148,11 +90,9 @@ class Robot {
                 break;
             case "WEST":
                 this.robotDirection = "SOUTH";
-                break;
             }
 
             console.log(`Robot is now facing ${this.robotDirection}!`);
-
     }
 
     right() {
@@ -174,7 +114,7 @@ class Robot {
                 break;
             case "WEST":
                 this.robotDirection = "NORTH";
-                break;
+
             }
 
             console.log(`Robot is now facing ${this.robotDirection}!`);
@@ -191,11 +131,60 @@ class Robot {
 
 function run() {
     let jimmy = new Robot();
-    jimmy.place([4,4], 'NORTH', jimmy.allowedDirections);
+    //First test
+    jimmy.place([0,0], 'NORTH', jimmy.allowedDirections);
+    jimmy.move();
+    console.log('Test 1: Expected output: 0,1,NORTH');
+    jimmy.report();
+
+    // //Second test
+    jimmy.place([0,0], 'NORTH', jimmy.allowedDirections);
+    jimmy.left();
+    console.log('Test 2: Expected output: 0,0,WEST');
+    jimmy.report();
+
+    // //Third test
+    jimmy.place([1,2], 'EAST', jimmy.allowedDirections);
+    jimmy.move();
     jimmy.move();
     jimmy.left();
     jimmy.move();
+    console.log('Test 3: Expected output: 3,3,NORTH');
     jimmy.report();
+
+    //CUSTOM TESTS (uncomment to test)
+
+    //Placing the robot on an invalid square
+
+    /*
+    jimmy.place([5,-5], 'EAST', jimmy.allowedDirections);
+    jimmy.report();
+    */
+
+    //Moving the robot off the board
+    
+    /*
+    jimmy.place([4,4], 'NORTH', jimmy.allowedDirections);
+    jimmy.move();
+    jimmy.report();
+    jimmy.left();
+    jimmy.move();
+    jimmy.report();
+    */
+
+    //Multiple PLACE commands
+
+    /*
+    jimmy.place([3,1], 'WEST', jimmy.allowedDirections);
+    jimmy.move();
+    jimmy.report();
+    jimmy.place([1,4], 'SOUTH', jimmy.allowedDirections);
+    jimmy.move();
+    jimmy.report();
+    */
+
+
+
 }
 
 run();
