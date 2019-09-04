b2a1b3e7754f027f242dde2c33a285fce220d900
Luke Turner <luke@lukedturner.com>
2019-09-04 13:56:51 +1000

    iteration 1 done

--- robot.js
+++ robot.js
@@ -15,9 +15,9 @@ class Robot {
         let allowedDirections = ["NORTH", "SOUTH", "EAST", "WEST"]; 
 
         //Check if position is on the table
-        //If first and second position are both between 0 and 4 and the direction is valid, let it go through
+        //If first and second position are both between 0 and 5 and the direction is valid, let it go through
         if (
-            (position[0] >=0 && position[0] <=4 && position[1] >= 0 && position[1] <=4)
+            (position[0] >=0 && position[0] <=5 && position[1] >= 0 && position[1] <=5)
             &&
             (allowedDirections.includes(direction))) {
                 //Set positions
@@ -41,19 +41,19 @@ class Robot {
         }
 
         //1. Determine position and direction.
-        //2. Ensure that positional values are not 4.
+        //2. Ensure that positional values are not 5.
         //3. Increase the x or y value by one, depending on these factors.
 
         //Move position
         switch (this.robotDirection) {
             case "NORTH":
-                (this.robotPosition[1] === 4) ? this.fallOffError("NORTH") : (this.robotPosition[1] ++, moved = true);
+                (this.robotPosition[1] === 5) ? this.fallOffError("NORTH") : (this.robotPosition[1] ++, moved = true);
                 break;
             case "SOUTH":
                 (this.robotPosition[1] === 0) ? this.fallOffError("SOUTH") : (this.robotPosition[1]--, moved = true);
                 break;
             case "EAST":
-                (this.robotPosition[0] === 4) ? this.fallOffError("EAST") : (this.robotPosition[0]++, moved = true);
+                (this.robotPosition[0] === 5) ? this.fallOffError("EAST") : (this.robotPosition[0]++, moved = true);
                 break;
             case "WEST":
                 (this.robotPosition[0] === 0) ? this.fallOffError("WEST") : (this.robotPosition[0]--, moved = true);
@@ -132,25 +132,25 @@ class Robot {
 function run() {
     let jimmy = new Robot();
     //First test
-    jimmy.place([0,0], 'NORTH', jimmy.allowedDirections);
+    jimmy.place([5,5], 'NORTH', jimmy.allowedDirections);
     jimmy.move();
-    console.log('Test 1: Expected output: 0,1,NORTH');
-    jimmy.report();
+    // console.log('Test 1: Expected output: 0,1,NORTH');
+    // jimmy.report();
 
-    // //Second test
-    jimmy.place([0,0], 'NORTH', jimmy.allowedDirections);
-    jimmy.left();
-    console.log('Test 2: Expected output: 0,0,WEST');
-    jimmy.report();
+    // // //Second test
+    // jimmy.place([0,0], 'NORTH', jimmy.allowedDirections);
+    // jimmy.left();
+    // console.log('Test 2: Expected output: 0,0,WEST');
+    // jimmy.report();
 
-    // //Third test
-    jimmy.place([1,2], 'EAST', jimmy.allowedDirections);
-    jimmy.move();
-    jimmy.move();
-    jimmy.left();
-    jimmy.move();
-    console.log('Test 3: Expected output: 3,3,NORTH');
-    jimmy.report();
+    // // //Third test
+    // jimmy.place([1,2], 'EAST', jimmy.allowedDirections);
+    // jimmy.move();
+    // jimmy.move();
+    // jimmy.left();
+    // jimmy.move();
+    // console.log('Test 3: Expected output: 3,3,NORTH');
+    // jimmy.report();
 
     //CUSTOM TESTS (uncomment to test)
 
@@ -186,5 +186,9 @@ function run() {
 
 
 }
+
+// Iteration 1 - we need to increase the table size to 6x6
+// What needs to happen here?
+//  Move conditionals need to be changed from 4 to 5
 
 run();
